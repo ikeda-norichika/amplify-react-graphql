3ffb041aa45cd5a789ce882b70c2f68260a4e917
ikeda-norichika <norichika-ikeda@telema.jp>
2024-09-09 16:20:22 +0900

    add auth

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -6,8 +6,6 @@ import { defineAuth } from '@aws-amplify/backend';
  */
 export const auth = defineAuth({
   loginWith: {
-    email: {
-      verificationEmailSubject: 'Welcome! Verify your email!'
-    }
-  },
+    email: true
+  }
 });
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Amplify } from "aws-amplify"
 import App from './App.tsx'
 import './index.css'
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: "ap-northeast-1_C00YkVitq",
+      userPoolClientId: "37duv6oakatjgfh3u17lsrncv6",
+      loginWith: {
+        email: true
+      }
+    },
+  }
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
