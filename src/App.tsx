3d4614aedaac5befcfec05e6504aa73ae6476454
ikeda-norichika <norichika-ikeda@telema.jp>
2024-09-10 13:10:15 +0900

    change auth

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,6 @@ import {
   Card,
 } from "@aws-amplify/ui-react";
 import '@aws-amplify/ui-react/styles.css';
-import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
-Amplify.configure(outputs);
 import './App.css'
 
 function App() {
