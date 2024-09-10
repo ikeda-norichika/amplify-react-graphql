5ec8ee4169a106a6b7efd8261dedaba231fbb81a
ikeda-norichika <norichika-ikeda@telema.jp>
2024-09-10 13:42:05 +0900

    change auth

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,22 @@
 import '@aws-amplify/ui-react/styles.css';
 import { ThemeProvider } from '@aws-amplify/ui-react';
 import { createRoot } from 'react-dom/client'
-import { Amplify } from "aws-amplify"
+// import { Amplify } from "aws-amplify"
 import App from './App.tsx'
 import './index.css'
 
-Amplify.configure({
-  Auth: {
-    Cognito: {
-      userPoolId: "ap-northeast-1_C00YkVitq",
-      userPoolClientId: "37duv6oakatjgfh3u17lsrncv6",
-      loginWith: {
-        username: true,
-        email: true
-      }
-    },
-  }
-})
+// Amplify.configure({
+//   Auth: {
+//     Cognito: {
+//       userPoolId: "ap-northeast-1_C00YkVitq",
+//       userPoolClientId: "37duv6oakatjgfh3u17lsrncv6",
+//       loginWith: {
+//         username: true,
+//         email: true
+//       }
+//     },
+//   }
+// })
 
 createRoot(document.getElementById('root')!).render(
   <ThemeProvider>
