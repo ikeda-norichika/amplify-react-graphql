54bde3c18464721fac86df393b318af48480a4e0
ikeda-norichika <norichika-ikeda@telema.jp>
2024-09-10 09:43:26 +0900

    add Signin

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
-import { Authenticator } from '@aws-amplify/ui-react';
+import reactLogo from "./assets/react.svg";
+import viteLogo from "../public/vite.svg";
+import {
+  Authenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card,
+} from "@aws-amplify/ui-react";
 import '@aws-amplify/ui-react/styles.css';
 import './App.css'
 
 function App() {
   return (
     <Authenticator>
-      {({ signOut, user }) => (
-        <main>
-          <h1>Hello {user?.username}</h1>
-          <button onClick={signOut}>Sign out</button>
-        </main>
+      {({ signOut}) => (
+        <View className="App">
+        <Card>
+          <div>
+          <Image src={reactLogo} className="App-logo" alt="logo" />
+          <Image src={viteLogo} className="App-logo" alt="logo" />
+          </div>
+          <Heading level={1}>We now have Auth!</Heading>
+        </Card>
+        <Button onClick={signOut}>Sign Out</Button>
+      </View>
       )}
     </Authenticator>
   )
